bde0facec64561ef36226584df78bac8239edfd1
alan-X73 <38806811+alan-X73@users.noreply.github.com>
2025-06-21 19:35:07 +0200

    Add files via upload

---
+++ website/scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const mailButton = document.querySelector('.chat-btn');
+    mailButton.addEventListener('click', function() {
+        window.location.href = "mailto:nick.teichmann@icloud.com";
+    });
+});
+
+
+window.addEventListener('scroll', () => {
+    const section = document.querySelector('.blog-section');
+    const scrollY = window.scrollY;
+    const newSize = 18 + Math.min(scrollY / 50, 1.5); // max 5px größer
+    section.style.fontSize = `${newSize}px`;
+});
